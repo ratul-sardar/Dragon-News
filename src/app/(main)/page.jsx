03ecd009fe5275257ea55624d49a1622e39443d6
ratul-sardar <ratulsardar4747@gmail.com>
2026-04-25 22:39:32 +0600

    added the async function for the catagory data fetch

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,6 +1,21 @@
-export default function Home() {
+async function getCatagorys() {
+  const url = "https://openapi.programming-hero.com/api/news/categories";
+
+  const res = await fetch(url);
+  const data = await res.json();
+
+  const {
+    data: { news_category },
+  } = data;
+
+  return news_category;
+}
+
+export default async function Home() {
+  console.log(await getCatagorys());
+
   return (
-    <section className="cssContainer grid justify-center">
+    <section className="cssContainer grid justify-center py-12 ">
       <h2 className="">Hi, from News page.</h2>
     </section>
   );
