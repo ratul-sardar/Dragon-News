1898604c59911d8226678e716d544e971298c358
ratul-sardar <ratulsardar4747@gmail.com>
2026-04-28 06:28:04 +0600

    added the auth for the signup page but dosen't work yet

--- src/app/(auth)/signup/page.jsx
+++ src/app/(auth)/signup/page.jsx
@@ -1,11 +1,25 @@
 "use client";
 
+import { authClient } from "@/lib/auth-client";
 import { Button } from "@heroui/react";
+import { error } from "better-auth/api";
 import Link from "next/link";
 
 async function handleSubmit(e) {
   e.preventDefault();
-  console.log(e.target.email.value);
+  const formData = new FormData(e.currentTarget);
+  const formObj = Object.fromEntries(formData);
+  const { name, email, password } = formObj;
+  console.log(formObj);
+
+  const { data, error } = await authClient.signUp.email({
+    name, // required
+    email, // required
+    password, // required
+    callbackURL: "/",
+  });
+
+  console.log("error: ", error);
 }
 
 export default function Signup() {
@@ -13,10 +27,22 @@ export default function Signup() {
     <>
       <section className="">
         <div className="cssContainer min-h-[80vh] py-8 flex flex-col items-center justify-center">
-          <h2 className="mb-6">Hi, from Login page</h2>
+          <h2 className="mb-6">Hi, from Signup page</h2>
 
           <div className="rounded-xl p-6 space-y-3 border border-gray-600">
             <form onSubmit={handleSubmit}>
+              {/* Email input*/}
+              <fieldset className="mb-4">
+                <legend className="mb-2">Name</legend>
+                <input
+                  type="text"
+                  name="name"
+                  id="userName"
+                  required
+                  className="border-2 border-lime-500"
+                />
+              </fieldset>
+
               {/* Email input*/}
               <fieldset className="mb-4">
                 <legend className="mb-2">Email</legend>
@@ -28,6 +54,7 @@ export default function Signup() {
                   className="border-2 border-lime-500"
                 />
               </fieldset>
+
               {/* Password input*/}
               <fieldset className="mb-8">
                 <legend className="mb-2">Password</legend>
@@ -47,8 +74,8 @@ export default function Signup() {
 
             <p className="">
               Or{" "}
-              <Link href={"/signup"} className="text-blue-500">
-                signup
+              <Link href={"/login"} className="text-blue-500">
+                Login
               </Link>
             </p>
           </div>
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,10 +1,11 @@
+import dns from "node:dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient(
-  "mongodb+srv://UserForDragonNews:RdapTktauxdjmKqL@cluster0.m0myjlf.mongodb.net/?appName=Cluster0",
-);
+const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db("dragon news user data");
 
 export const auth = betterAuth({
